4cc21b62198ed50d2d7907b5b9495fc663e85646
hansdav <hdavidson21@gmail.com>
2023-04-20 18:45:12 +0200

    background photo size changed

--- client/src/components/Miniblog/miniblog.jsx
+++ client/src/components/Miniblog/miniblog.jsx
@@ -1,14 +1,15 @@
-const miniBlog = ({ title, author, date, text }) => {
+const miniBlog = ({ title, author, date, post }) => {
 	return (
 		<div className="container">
 			<img
-				id="wallpaper"
+				className="wallpaper"
+				style={{ width: "20%" }}
 				src="../src/assets/office-table-with-cup-coffee-keyboard-notepad.jpg"
 			></img>
 			<div className="title">{title}</div>
 			<div className="author">By: {author}</div>
 			<div className="date">Date: {date}</div>
-			<div className="text-content">{text}</div>
+			<div className="text-content">{post}</div>
 		</div>
 	);
 };

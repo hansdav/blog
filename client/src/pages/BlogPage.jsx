52ab56be5fdd5949728661b2f55fe2c397157ead
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-04-20 18:56:32 +0200

    .

--- client/src/pages/BlogPage.jsx
+++ client/src/pages/BlogPage.jsx
@@ -1,18 +1,18 @@
-import MiniBlog from "../components/Miniblog/MiniBlog.jsx";
+import MiniBlog from '../components/Miniblog/MiniBlog.jsx';
 
 const BlogPage = ({ blogs }) => {
-	return (
-		<div className="BlogPage">
-			{blogs.map((blog) => (
-				<MiniBlog
-					title={blog.title}
-					author={blog.author}
-					post={blog.post}
-					date={blog.date}
-				></MiniBlog>
-			))}
-		</div>
-	);
+  return (
+    <div className='BlogPage'>
+      {blogs.map((blog) => (
+        <MiniBlog
+          title={blog.title}
+          author={blog.author}
+          post={blog.post}
+          date={blog.date}
+        ></MiniBlog>
+      ))}
+    </div>
+  );
 };
 
 export default BlogPage;

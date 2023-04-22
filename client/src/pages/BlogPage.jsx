2ba57f75d09f01a1eab3e77a1b69b8109c29e13b
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-04-22 10:33:08 +0200

    likes and other shit

--- client/src/pages/BlogPage.jsx
+++ client/src/pages/BlogPage.jsx
@@ -1,19 +1,22 @@
-import MiniBlog from "../components/MiniBlog/MiniBlog.jsx";
-import "./BlogPage.css";
+import MiniBlog from '../components/MiniBlog/MiniBlog.jsx';
+import './BlogPage.css';
 
 const BlogPage = ({ blogs }) => {
-	return (
-		<div className="BlogPage">
-			{blogs.map((blog) => (
-				<MiniBlog
-					title={blog.title}
-					author={blog.author}
-					post={blog.post}
-					date={blog.date}
-				></MiniBlog>
-			))}
-		</div>
-	);
+  return (
+    <div className='BlogPage'>
+      {blogs.map((blog) => (
+        <MiniBlog
+          key={blog._id}
+          id={blog._id}
+          title={blog.title}
+          author={blog.author}
+          likes={blog.likes}
+          post={blog.post}
+          date={blog.date}
+        ></MiniBlog>
+      ))}
+    </div>
+  );
 };
 
 export default BlogPage;

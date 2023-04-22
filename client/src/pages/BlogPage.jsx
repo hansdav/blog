5814fa0f767ac9b8526e1a7f960087bde666bbbd
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2023-04-22 14:03:12 +0200

    fix 3

--- client/src/pages/BlogPage.jsx
+++ client/src/pages/BlogPage.jsx
@@ -1,35 +1,37 @@
-import MiniBlog from "../components/MiniBlog/MiniBlog.jsx";
-import QueryPanel from "../components/QueryPanel/QueryPanel.jsx";
-import "./BlogPage.css";
+import MiniBlog from '../components/MiniBlog/MiniBlog.jsx';
+import QueryPanel from '../components/QueryPanel/QueryPanel.jsx';
+import './BlogPage.css';
 
 const BlogPage = ({
-	blogs,
-	sortDate,
-	sortLikes,
-	filterAuthor,
-	filterTitle,
+  blogs,
+  sortDate,
+  sortLikes,
+  filterAuthor,
+  filterTitle,
 }) => {
-	return (
-		<>
-			<QueryPanel
-				sortDate={sortDate}
-				sortLikes={sortLikes}
-				filterAuthor={filterAuthor}
-				filterTitle={filterTitle}
-			></QueryPanel>
-			<div className="BlogPage">
-				{blogs.map((blog) => (
-					<MiniBlog
-						key={blog._id}
-						title={blog.title}
-						author={blog.author}
-						post={blog.post}
-						date={blog.created.slice(0, 10)}
-					></MiniBlog>
-				))}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <QueryPanel
+        sortDate={sortDate}
+        sortLikes={sortLikes}
+        filterAuthor={filterAuthor}
+        filterTitle={filterTitle}
+      ></QueryPanel>
+      <div className='BlogPage'>
+        {blogs.map((blog) => (
+          <MiniBlog
+            key={blog._id}
+            id={blog._id}
+            title={blog.title}
+            author={blog.author}
+            post={blog.post}
+            likes={blog.likes}
+            date={blog.created.slice(0, 10)}
+          ></MiniBlog>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default BlogPage;
